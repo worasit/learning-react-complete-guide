99a83817a58fe2537f5dc1476b1419ad33888b2b
worasit <worasit.daimongkol@thoughtworks.com>
2022-01-22 16:42:55 +0700

    applied date conversion logic to a component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,9 +2,16 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   const { title, amount, date } = props;
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{date.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount.toString()}</div>
