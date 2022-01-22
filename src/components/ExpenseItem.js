d1c5fd0784b08d88b0f02bfe2d845beb7c1aa545
worasit <worasit.daimongkol@thoughtworks.com>
2022-01-22 16:19:56 +0700

    applied dynamic data using expression statement

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expensePrice = 294.68;
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice.toString()}</div>
       </div>
     </div>
   );
