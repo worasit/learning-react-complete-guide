ed461aa53a072492e63aa0c1b2e9bf40a897d452
worasit <worasit.daimongkol@thoughtworks.com>
2022-01-23 06:04:47 +0700

    added expense form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -31,6 +32,7 @@ const App = () => {
   return (
     <div>
       <h1>Let's get started</h1>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
