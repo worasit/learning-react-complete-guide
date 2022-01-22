985def1c795739ef7d16c67b9a1a14c3ea9b0b45
worasit <worasit.daimongkol@thoughtworks.com>
2022-01-23 06:13:33 +0700

    added titleChangedHandler to listen to title change

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,12 +3,15 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  const titleChangedHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangedHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
