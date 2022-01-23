53dd42419e4ae5c735b30a4ea9682280d709314e
worasit <worasit.daimongkol@thoughtworks.com>
2022-01-24 06:35:52 +0700

    fixing a bug, sum value of amount per month

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -63,7 +63,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
