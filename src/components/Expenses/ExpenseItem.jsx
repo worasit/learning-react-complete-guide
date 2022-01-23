89f80077cac033f1e8e8de882c77ed4e94e9b8a4
worasit <worasit.daimongkol@thoughtworks.com>
2022-01-23 13:21:43 +0700

    removed demo state

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,23 +4,14 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  let { amount, date } = props;
-  const [title, setTitle] = useState(props.title);
   console.log("ExpenseItem evaluated by React.");
-
-  const onClickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount.toString()}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount.toString()}</div>
       </div>
-      <button onClick={onClickHandler}>Change a title</button>
     </Card>
   );
 };
